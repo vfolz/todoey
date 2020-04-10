f84e2bfb88cc8607768cd42344c3ac33fd63a5ea
Victor Folz <victorfolz@gmail.com>
2020-04-09 17:05:32 -0700

    initial commit

---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Padding(
+        padding: const EdgeInsets.only(
+            top: 60.0, right: 30.0, left: 30.0, bottom: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CircleAvatar(
+              child: Icon(Icons.list,size: 30.0,color: Colors.lightBlueAccent,),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),SizedBox(height: 10.0),
+            Text('Todoey',
+            style: TextStyle(color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,),),
+            Text('12 Taks',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              
+
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
