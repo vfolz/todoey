ef60a34abcedb2236643c7f021a9f2173d82e927
vfolz <victorfolz@gmail.com>
2020-04-09 23:59:50 -0700

    Finished primary layout

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -5,31 +5,59 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Padding(
-        padding: const EdgeInsets.only(
-            top: 60.0, right: 30.0, left: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              child: Icon(Icons.list,size: 30.0,color: Colors.lightBlueAccent,),
-              backgroundColor: Colors.white,
-              radius: 30.0,
-            ),SizedBox(height: 10.0),
-            Text('Todoey',
-            style: TextStyle(color: Colors.white,
-            fontSize: 50.0,
-            fontWeight: FontWeight.w700,),),
-            Text('12 Taks',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-              
-
-            ),)
-          ],
-        ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 60.0, right: 30.0, left: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Taks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40))),
+            ),
+          )
+        ],
       ),
     );
   }
